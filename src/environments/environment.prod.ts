879e1329885d1cee73a12126635040a85d6eeaa3
Vladyslav <vk@asseco.dk>
2021-09-15 15:18:44 +0200

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBGH-umL10BG9f1jbtahO324inKwV5Fnbw',
+  fbDbUrl: 'https://angular-hus-blog-default-rtdb.europe-west1.firebasedatabase.app'
+};
